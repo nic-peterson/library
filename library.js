e2ff2ad924e2d095b22e2fdaecd47804f12febf3
Nic Peterson <nic@peterson.to>
2022-01-20 18:45:36 -0800

    Starting the html/css from scratch

--- library.js
+++ library.js
@@ -1,7 +1,8 @@
-
-
 let myLibrary = [];
 
+/*
+Create a book object and define the functions to add, remove, and display books to the library.
+*/
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -21,19 +22,32 @@ function addBookToLibrary(book, arr) {
 }
 
 function removeBookfromLibrary(index, arr) {
-    return arr.splice(index, 1);
+  return arr.splice(index, 1);
 }
 
 function displayBooks(arr) {
-    arr.forEach(book => console.log(book.info()));
+  const cardContainer = document.querySelector(".card-container");
+  arr.forEach((book) => {
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.setAttribute("id", `${book.title}`);
+    card.textContent = `${book.info()}`;
+    cardContainer.appendChild(card);
+  });
+
+  // console.log(book.info())
 }
 
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-let gatsby = new Book ("The Great Gatsby", "F. Scott Fitzgerald", 350, true);
-let catch22 = new Book ("Catch-22", "Joseph Heller", 400, true);
-let theStranger = new Book("The Stranger", "Albert Camus", 250, true);
-let hamlet = new Book("Hamlet", "William Shakespeare", 150, true);
+
+/*
+Create books and display
+*/
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const gatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 350, true);
+const catch22 = new Book("Catch-22", "Joseph Heller", 400, true);
+const theStranger = new Book("The Stranger", "Albert Camus", 250, true);
+const hamlet = new Book("Hamlet", "William Shakespeare", 150, true);
 
 addBookToLibrary(theHobbit, myLibrary);
 addBookToLibrary(gatsby, myLibrary);
@@ -41,9 +55,4 @@ addBookToLibrary(catch22, myLibrary);
 addBookToLibrary(theStranger, myLibrary);
 addBookToLibrary(hamlet, myLibrary);
 
-console.log(theHobbit.info());
-
 displayBooks(myLibrary);
-//console.log(myLibrary);
-
-
